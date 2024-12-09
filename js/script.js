d9edf293ce60dc51e48521e3b57f02a2ca2472af
BogdanPelenskiy <80599403+BogdanPelenskiy@users.noreply.github.com>
2024-12-09 20:52:46 +0000

    Update JS

--- js/script.js
+++ js/script.js
@@ -1,37 +1,31 @@
-document.querySelector('#push').onclick = function
-() {
-    if(document.querySelector('#newtask input')
-    .value.length == 0){
-        alert("Please Enter a Tasks")
-    }
-    else{
-        document.querySelector('#tasks').innerHTML
-        += `
+document.querySelector(".text").innerText += `To Do List`;
+
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("Please Enter a Tasks");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
             <div class="task">
                 <span id="taskname">
-                    ${document.querySelector
-                    ('#newtask input').value}
+                    ${document.querySelector("#newtask input").value}
                 </span>
                 <button class="delete">
                 <svg class="ico__delete" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"/></svg>
                 </button>
             </div>
-        `; 
-        var current_tasks = document.
-        querySelectorAll(".delete");
-        for(var i = 0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function() {
-                this.parentNode.remove();
-            }
-        }
+        `;
+    var current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
     }
-        var tasks = document.
-        querySelectorAll(".task");
-        for(var i = 0; i < tasks.length; i++){
-            tasks[i].onclick = function(){
-                this.classList.toggle('completed')
-            }
-        }
-    document.querySelector("#newtask input").
-    value = ""
-}
+  }
+  var tasks = document.querySelectorAll(".task");
+  for (var i = 0; i < tasks.length; i++) {
+    tasks[i].onclick = function () {
+      this.classList.toggle("completed");
+    };
+  }
+  document.querySelector("#newtask input").value = "";
+};
